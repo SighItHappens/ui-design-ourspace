25063846d076e0e56226ab9fd2954424cb264255
becks045 <becks045@umn.edu>
2020-04-01 17:17:52 -0500

    Refactor - Set list for recyclerview in HomeViewModel  - Better way to handle mutable data

--- ourspace/app/src/main/java/com/project/ourspace/ui/home/HomeFragment.java
+++ ourspace/app/src/main/java/com/project/ourspace/ui/home/HomeFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,37 +30,27 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
 
-        HomeViewModel homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+//        HomeViewModel homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         recyclerView = (RecyclerView) root.findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        tileList = new ArrayList<>();
+        // Create the observer which updates the UI.
+        final Observer<List<Tile>> tileObserver = new Observer<List<Tile>>() {
+            @Override
+            public void onChanged(@Nullable final List<Tile> tileList) {
+                // Update the UI, in this case, a TextView.
+                //creating recyclerview adapter
+                TileAdapter adapter = new TileAdapter(getActivity(), tileList);
 
-
-        //adding some items to our list
-        tileList.add(
-                new Tile(
-                        1,
-                        "Item1"));
-
-        tileList.add(
-                new Tile(
-                        1,
-                        "Item2"));
-
-        tileList.add(
-                new Tile(
-                        1,
-                        "Item2"));
-
-        //creating recyclerview adapter
-        TileAdapter adapter = new TileAdapter(getActivity(), tileList);
-
-        //setting adapter to recyclerview
-        recyclerView.setAdapter(adapter);
+                //setting adapter to recyclerview
+                recyclerView.setAdapter(adapter);
+            }
+        };
+        homeViewModel.getList().observe(getActivity(), tileObserver);
         return root;
     }
 }
--- ourspace/app/src/main/java/com/project/ourspace/ui/home/HomeViewModel.java
+++ ourspace/app/src/main/java/com/project/ourspace/ui/home/HomeViewModel.java
@@ -4,16 +4,46 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<List<Tile>> mList;
+    private List<Tile> tileList;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
+        mList = new MutableLiveData<>();
+
+        tileList = new ArrayList<>();
+
+
+        //adding some items to our list
+        tileList.add(
+                new Tile(
+                        1,
+                        "Item1"));
+
+        tileList.add(
+                new Tile(
+                        1,
+                        "Item2"));
+
+        tileList.add(
+                new Tile(
+                        1,
+                        "Item2"));
+        mList.setValue(tileList);
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<List<Tile>> getList() {
+        return mList;
     }
 }
