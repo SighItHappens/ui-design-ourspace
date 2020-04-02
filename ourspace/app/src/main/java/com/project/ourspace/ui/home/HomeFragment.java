1c577be7113f8233a40e3743a94afd7097cc585f
Suresh <suresh.siddharth@gmail.com>
2020-04-01 19:31:58 -0500

    Implements the Add button, and other QoL changes
    This commit adds the changes needed to implement the 'Add' button on the
    home screen. It also includes changes to the theme of the application
    and defines the material colour pallete for future use

--- ourspace/app/src/main/java/com/project/ourspace/ui/home/HomeFragment.java
+++ ourspace/app/src/main/java/com/project/ourspace/ui/home/HomeFragment.java
@@ -12,16 +12,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.project.ourspace.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -30,6 +30,7 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
         return root;
     }
 }
