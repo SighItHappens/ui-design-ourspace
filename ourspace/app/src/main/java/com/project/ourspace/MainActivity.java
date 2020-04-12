8def023c35b7db21bca735e8cecd98cc77b7ac5d
hans4837 <hans4837@umn.edu>
2020-04-12 14:25:20 -0500

    initial tv value commital

--- ourspace/app/src/main/java/com/project/ourspace/MainActivity.java
+++ ourspace/app/src/main/java/com/project/ourspace/MainActivity.java
@@ -74,14 +74,16 @@ public class MainActivity extends AppCompatActivity {
 
         final Toast toast = Toast.makeText(getApplicationContext(), "Replace with your own TV action", Toast.LENGTH_SHORT);
         final Intent createNoteIntent = new Intent(this, CreateNoteActivity.class);
+        final Intent createTelevisionIntent = new Intent(this, CreateTelevisionActivity.class);
 
         speedDialView.setOnActionSelectedListener(new SpeedDialView.OnActionSelectedListener() {
             @Override
             public boolean onActionSelected(SpeedDialActionItem speedDialActionItem) {
                 switch (speedDialActionItem.getId()) {
                     case R.id.fab_add_tv_show:
-                        toast.setText("Custom TV action");
-                        toast.show();
+                        startActivity(createTelevisionIntent);
+//                        toast.setText("Custom TV action");
+//                        toast.show();
                         return false;
                     case R.id.fab_add_music:
                         toast.setText("Custom Music action");
