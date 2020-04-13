96737e24830f06f1e84a5b9e9985ec23fbd9a07d
hans4837 <hans4837@umn.edu>
2020-04-13 17:10:26 -0500

    implemented "search" functionality as a dropdown menu

--- ourspace/app/src/main/java/com/project/ourspace/CreateTelevisionActivity.java
+++ ourspace/app/src/main/java/com/project/ourspace/CreateTelevisionActivity.java
@@ -2,25 +2,49 @@ package com.project.ourspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.SearchView;
 
 public class CreateTelevisionActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.MESSAGE";
+
+    AutoCompleteTextView autocomplete;
+    String[] arr = {"Breaking Bad","Friends","Planet Earth"};
+   // String[] arr = { "Paries,France", "PA,United States","Parana,Brazil",
+   //         "Padua,Italy", "Pasadena,CA,United States"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_television);
+
+        autocomplete = (AutoCompleteTextView)
+                findViewById(R.id.autoCompleteTextView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this,android.R.layout.select_dialog_item, arr);
+
+        autocomplete.setThreshold(2);
+        autocomplete.setAdapter(adapter);
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+        // Get the SearchView and set the searchable configuration
+//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        SearchView searchView = (SearchView) findViewById(R.id.searchView);
+
+        // Assumes current activity is the searchable activity
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+//        searchView.setIconifiedByDefault(true); // Do not iconify the widget; expand it by default
+//        searchView.setSubmitButtonEnabled(true);
+//    return true;
+
+
     }
-}
+
