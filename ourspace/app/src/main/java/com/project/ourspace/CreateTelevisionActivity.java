56b223836c417c2fc57dea8c3512dab62e8363c9
hans4837 <hans4837@umn.edu>
2020-04-13 21:40:05 -0500

    hardcoded basic show creation logic. can't get images to show. don't know how to return season and episode values for display in final product on family wall

--- ourspace/app/src/main/java/com/project/ourspace/CreateTelevisionActivity.java
+++ ourspace/app/src/main/java/com/project/ourspace/CreateTelevisionActivity.java
@@ -9,12 +9,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SearchView;
 
 public class CreateTelevisionActivity extends AppCompatActivity {
 
     AutoCompleteTextView autocomplete;
+    Button searchButton;
+    Button addButton;
+    ImageView showImage0, showImage1, showImage2;
     String[] arr = {"Breaking Bad","Friends","Planet Earth"};
    // String[] arr = { "Paries,France", "PA,United States","Parana,Brazil",
    //         "Padua,Italy", "Pasadena,CA,United States"};
@@ -32,6 +37,60 @@ public class CreateTelevisionActivity extends AppCompatActivity {
 
         autocomplete.setThreshold(2);
         autocomplete.setAdapter(adapter);
+
+        showImage0 = (ImageView) findViewById(R.id.imageView0);
+        showImage1 = (ImageView) findViewById(R.id.imageView1);
+        showImage2 = (ImageView) findViewById(R.id.imageView2);
+
+        addButton = (Button) findViewById(R.id.button3);
+
+        searchButton = (Button) findViewById(R.id.button2);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              String x = autocomplete.getText().toString();
+              if(x.equals(arr[0])){
+                  // Breaking Bad
+                  addButton.setVisibility(View.VISIBLE);
+                  showImage0.setVisibility(View.VISIBLE);
+                  showImage1.setVisibility(View.GONE);
+                  showImage2.setVisibility(View.GONE);
+              } else if(x.equals(arr[1])){
+                  // Friends
+                  addButton.setVisibility(View.VISIBLE);
+                  showImage0.setVisibility(View.GONE);
+                  showImage1.setVisibility(View.VISIBLE);
+                  showImage2.setVisibility(View.GONE);
+              } else if(x.equals(arr[2])){
+                  // Planet Earth
+                  addButton.setVisibility(View.VISIBLE);
+                  showImage0.setVisibility(View.GONE);
+                  showImage1.setVisibility(View.GONE);
+                  showImage2.setVisibility(View.VISIBLE);
+              }
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(showImage0.getVisibility()==View.VISIBLE){
+                // Breaking Bad
+                    // return seasons = 5, episodes = 13
+                    // create TV show instance on Family Wall & return to home page
+                }else if(showImage1.getVisibility()==View.VISIBLE){
+                // Friends
+                    // return seasons = 10, episodes = 24
+                    // create TV show instance on Family Wall & return to home page
+                }else if(showImage2.getVisibility()==View.VISIBLE){
+                // Planet Earth
+                    // return seasons = 1, episodes = 11
+                    // create TV show instance on Family Wall & return to home page
+
+                }
+            }
+        });
+
     }
 
 
