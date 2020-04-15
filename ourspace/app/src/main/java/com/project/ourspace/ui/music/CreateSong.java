37f04f6a080b8860ec6741d9b74d449b3b8887aa
Sam Hamann <shamanphi@gmail.com>
2020-04-15 15:43:30 -0500

    Creating home view and tile usage

--- ourspace/app/src/main/java/com/project/ourspace/ui/music/CreateSong.java
+++ ourspace/app/src/main/java/com/project/ourspace/ui/music/CreateSong.java
@@ -6,18 +6,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.project.ourspace.MainActivity;
 import com.project.ourspace.R;
 import com.project.ourspace.data.LoginRepository;
 import com.project.ourspace.data.model.LoggedInUser;
+import com.project.ourspace.data.model.TileList;
+import com.project.ourspace.ui.home.HomeViewModel;
+import com.project.ourspace.ui.home.Tile;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CreateSong extends AppCompatActivity {
     private static final String TAG = "CreateSongActivity";
+
+    HomeViewModel homeViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +57,9 @@ public class CreateSong extends AppCompatActivity {
         }
 
         LoggedInUser userDetails = LoginRepository.getInstance().getUserDetails();
+
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        TileList.addItem(new Tile(2, userDetails.getDisplayName(), "14 Apr 2020", songNameString, artistNameString, songUrlString));
 
         Log.d(TAG, "Retrieved Note Owner: " + userDetails.getDisplayName());
         Log.d(TAG, "Retrieved Song Name: " + songNameString);
